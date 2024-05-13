71a8a5b3222e3d7f159e77a66cccfe4423215232
hedgehog001123 <hedgehog001123@gmail.com>
2024-05-13 21:06:41 +0900

    24/05/13_2

--- src/atcoder/abc/abc047/b/abc047_b.cpp
+++ src/atcoder/abc/abc047/b/abc047_b.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-    
+    int w, h, n;
+    cin >> w >> h >> n;
+    vector<int> x(n), y(n), a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i] >> a[i];
+    }
+    int l = 0, r = w, d = 0, u = h;
+    for(int i = 0; i < n; i++) {
+        if(a[i] == 1) l = max(l, x[i]);
+        if(a[i] == 2) r = min(r, x[i]);
+        if(a[i] == 3) d = max(d, y[i]);
+        if(a[i] == 4) u = min(u, y[i]);
+    }
+
+    // cout << l << " " << r << " " << u << " " << d <<endl;
+    cout << max(r - l, 0) * max(u - d, 0) << endl;
 
     return 0;
 }
--- src/atcoder/abc/abc091/b/abc091_b.cpp
+++ src/atcoder/abc/abc091/b/abc091_b.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 int main() {
-    
+    int n, m;
+    cin >> n;
+    vector<string> s(n);
+    for(int i = 0; i < n; i++) cin >> s[i];
+    cin >> m;
+    vector<string> t(m);
+    for(int i = 0; i < m; i++) cin >> t[i];
+
+    int ans = 0;
+    map<string, int> ma;
+    for(int i = 0; i < m; i++) {
+        ma[t[i]]--;
+    }
+    for(int i = 0; i < n; i++) {
+        ma[s[i]]++;
+        ans = max(ans, ma[s[i]]);
+    }
+
+    cout << ans << endl;
 
     return 0;
 }

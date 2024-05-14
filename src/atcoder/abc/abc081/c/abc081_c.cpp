05cbeca8a57832886cdf47677ccf49212e0317a4
hedgehog001123 <hedgehog001123@gmail.com>
2024-05-14 22:26:11 +0900

    24/05/14

--- src/atcoder/abc/abc081/c/abc081_c.cpp
+++ src/atcoder/abc/abc081/c/abc081_c.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    vector<int> backet(n + 10);
+    for(int i = 0; i < n; i++) {
+        backet[a[i]]++;
+    }
+    sort(backet.begin(), backet.end(), greater<int>());
+    int ans = 0;
+    for(int i = k; i < n + 5; i++) {
+        ans += backet[i];
+    }
     
+    cout << ans << endl;
 
     return 0;
 }
--- src/atcoder/abc/abc085/c/abc085_c.cpp
+++ src/atcoder/abc/abc085/c/abc085_c.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    int n, y;
+    cin >> n >> y;
+
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; i + j <= n; j++) {
+            if(i * 10000 + j * 5000 + (n - i - j) * 1000 == y){
+                cout << i << " " << j << " " << n - i - j << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "-1 -1 -1" <<endl;
 
     return 0;
 }

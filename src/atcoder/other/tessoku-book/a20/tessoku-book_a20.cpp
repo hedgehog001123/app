272d88ecc091696dc0d27d3c2f24f1f977e9273e
hedgehog001123 <hedgehog001123@gmail.com>
2024-05-23 21:27:49 +0900

    24/05/23

--- src/atcoder/other/tessoku-book/a20/tessoku-book_a20.cpp
+++ src/atcoder/other/tessoku-book/a20/tessoku-book_a20.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[2010][2010];
+
 int main() {
-    
+    string s, t;
+    cin >> s;
+    int n = s.size();
+    cin >> t;
+    int m = t.size();
+
+    dp[0][0] = 0;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (i >= 1 && j >= 1) {
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1] + 1});
+                } 
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+            else if (i >= 1) {
+                dp[i][j] = dp[i-1][j];
+            }
+            else if (j >= 1) {
+                dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+
+    cout << dp[n][m] << endl;
 
     return 0;
 }
--- src/atcoder/other/tessoku-book/b20/tessoku-book_b20.cpp
+++ src/atcoder/other/tessoku-book/b20/tessoku-book_b20.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[2009][2009];
+
 int main() {
-    
+    string s;
+    cin >> s;
+    int n = s.size();
+    string t;
+    cin >> t;
+    int m = t.size();
+
+    dp[0][0] = 0;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (i >= 1 && j >= 1 && s[i-1] == t[j-1]) {
+                dp[i][j] = min({dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1]});
+            }
+            else if (i >= 1 && j >= 1) {
+                dp[i][j] = min({dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + 1});
+            }
+            else if (i >= 1) {
+                dp[i][j] = dp[i-1][j] + 1;
+            }
+            else if (j >= 1) {
+                dp[i][j] = dp[i][j-1] + 1;
+            }
+        }
+    }
+
+    cout << dp[n][m] << endl;
 
     return 0;
 }

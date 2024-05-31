1a3ff051b7d801321f69559677c8e4bb8007625b
hedgehog001123 <hedgehog001123@gmail.com>
2024-05-31 21:07:25 +0900

    24/05/31

--- src/atcoder/other/tessoku-book/a38/tessoku-book_a38.cpp
+++ src/atcoder/other/tessoku-book/a38/tessoku-book_a38.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lim[400];
+
 int main() {
-    
+    int d, n;
+    cin >> d >> n;
+    vector <int> l(n), r(n), h(n);
+    for (int i = 0; i < n; i++) cin >> l[i] >> r[i] >> h[i];
+
+    for (int i = 0; i < 400; i++) lim[i] = 24;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = l[i]-1; j < r[i]; j++) {
+            lim[j] = min(lim[j], h[i]);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < d; i++) {
+        ans += lim[i];
+    }
+    cout << ans << endl;
 
     return 0;
 }
--- src/atcoder/other/tessoku-book/b38/tessoku-book_b38.cpp
+++ src/atcoder/other/tessoku-book/b38/tessoku-book_b38.cpp
@@ -2,7 +2,41 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    string s;
+    cin >> n >> s;
+    vector<int> m1(n), m2(n);
+
+    int streak_a = 1;
+    m1[0] = 1;
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == 'A') {
+            streak_a++;
+        }
+        else if (s[i] == 'B') {
+            streak_a = 1;
+        }
+        m1[i + 1] = streak_a;
+    }
+
+    int streak_b = 1;
+    m2[n-1] = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (s[i] == 'A') {
+            streak_b = 1;
+        }
+        else if (s[i] == 'B') {
+            streak_b++;
+        }
+        m2[i] = streak_b;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += max(m1[i], m2[i]);
+    }
+
+    cout << ans << endl;
 
     return 0;
 }

0ca6ef3859615bbdf7e7cf898df2f5022da2b685
hedgehog001123 <hedgehog001123@gmail.com>
2024-06-11 23:10:23 +0900

    24/06/11_2

--- src/atcoder/abc/abc042/a/abc042_a.cpp
+++ src/atcoder/abc/abc042/a/abc042_a.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (min({a, b, c}) == 5 && max({a, b, c}) == 7 && a + b + c == 17) cout << "YES" << endl;
+    else cout << "NO" << endl;
 
     return 0;
 }

72f1400c34bfc262c43a740c05af95bc0026c921
hedgehog001123 <hedgehog001123@gmail.com>
2024-07-21 22:39:28 +0900

    24/07/21

--- src/atcoder/abc/abc290/b/abc290_b.cpp
+++ src/atcoder/abc/abc290/b/abc290_b.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'o' && cnt < k) {
+            cout << 'o';
+            cnt++;
+        }
+        else cout << 'x';
+    }
+    cout << endl;
 
     return 0;
 }
--- src/atcoder/abc/abc294/b/abc294_b.cpp
+++ src/atcoder/abc/abc294/b/abc294_b.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
-    
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h, vector<int>(w));
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 0) {
+                cout << ".";
+            }
+            else {
+                cout << char('A' + a[i][j] - 1);
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }

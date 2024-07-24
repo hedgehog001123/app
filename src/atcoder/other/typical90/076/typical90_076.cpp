827d0062fba0ad3116d8567caed0666bd597e5fb
hedgehog001123 <hedgehog001123@gmail.com>
2024-07-24 19:18:04 +0900

    24/07/24

--- src/atcoder/other/typical90/076/typical90_076.cpp
+++ src/atcoder/other/typical90/076/typical90_076.cpp
@@ -2,7 +2,37 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    long long sum = 0;
+    for (int i = 0; i < n; i++) sum += a[i];
+
+    if (sum % 10 != 0) {
+        cout << "No" << endl;
+        return 0;
+    }
+    long long b = sum / 10;
+
+    int front = 0, back = 0;
+    long long tmpsum = 0;
+    int kaisu = 0;
+    while (kaisu <= 200000) {
+        kaisu++;
+        tmpsum += a[back%n];
+        back++;
+        if (tmpsum == b) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+        else if (tmpsum > b) {
+            tmpsum -= a[front%n];
+            front++;
+        }
+    }
+
+    cout << "No" << endl;
 
     return 0;
 }

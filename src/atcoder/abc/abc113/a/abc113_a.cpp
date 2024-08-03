6f5c4c35e9e7e416aa98257e15812bac2b249ad2
hedgehog001123 <hedgehog001123@gmail.com>
2024-08-03 22:14:58 +0900

    24/08/03

--- src/atcoder/abc/abc113/a/abc113_a.cpp
+++ src/atcoder/abc/abc113/a/abc113_a.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main(){
-    
+    int x, y;
+    cin >> x >> y;
+    cout << x + y / 2 << endl;
 
     return 0;
 }
--- src/atcoder/abc/abc133/a/abc133_a.cpp
+++ src/atcoder/abc/abc133/a/abc133_a.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main(){
-    
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << min(b, a * n) << endl;
 
     return 0;
 }
